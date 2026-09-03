2f5c00f209475c1e88d98bd0f7690f0345cafed3
Anthony Brown <tonybr777@gmail.com>
2026-09-03 02:21:54 -0500

    Add Publisher entity and Book-Publisher relationship

--- src/main/java/guru/springframework/spring7webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring7webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring7webapp.bootstrap;
 
 import guru.springframework.spring7webapp.domain.Author;
 import guru.springframework.spring7webapp.domain.Book;
+import guru.springframework.spring7webapp.domain.Publisher;
 import guru.springframework.spring7webapp.repositories.AuthorRepository;
 import guru.springframework.spring7webapp.repositories.BookRepository;
+import guru.springframework.spring7webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,52 +13,80 @@ import org.springframework.stereotype.Component;
  * Created by jt, Spring Framework Guru.
  */
 @Component
-public class BootstrapData implements CommandLineRunner {
+public class BootstrapData implements CommandLineRunner
+{
 
-    private final AuthorRepository authorRepository;
-    private final BookRepository bookRepository;
+  private final AuthorRepository authorRepository;
+  private final BookRepository bookRepository;
+  private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
-        this.authorRepository = authorRepository;
-        this.bookRepository = bookRepository;
-    }
+  public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository)
+  {
+    this.authorRepository = authorRepository;
+    this.bookRepository = bookRepository;
+    this.publisherRepository = publisherRepository;
+  }
 
-    @Override
-    public void run(String... args) throws Exception {
-        Author eric = new Author();
-        eric.setFirstName("Eric");
-        eric.setLastName("Evans");
+  @Override
+  public void run(String... args) throws Exception
+  {
 
-        Book ddd = new Book();
-        ddd.setTitle("Domain Driven Design");
-        ddd.setIsbn("123456");
+    // Add first publisher
+    Publisher addisonWesley = new Publisher();
+    addisonWesley.setPublisherName("Addison Wesley");
+    addisonWesley.setState("New York");
+    addisonWesley.setZip("11697");
+    Publisher addisonWesleySaved = publisherRepository.save(addisonWesley);
 
-        Author ericSaved = authorRepository.save(eric);
-        Book dddSaved = bookRepository.save(ddd);
+    // Add second publisher
+    Publisher wrox = new Publisher();
+    wrox.setPublisherName("Wrox");
+    wrox.setState("New York");
+    wrox.setZip("10001");
+    Publisher wroxSaved = publisherRepository.save(wrox);
 
-        Author rod = new Author();
-        rod.setFirstName("Rod");
-        rod.setLastName("Johnson");
+    // Add first author
+    Author eric = new Author();
+    eric.setFirstName("Eric");
+    eric.setLastName("Evans");
 
-        Book noEJB = new Book();
-        noEJB.setTitle("J2EE Development without EJB");
-        noEJB.setIsbn("54757585");
+    // Add first book
+    Book ddd = new Book();
+    ddd.setTitle("Domain Driven Design");
+    ddd.setIsbn("123456");
+    ddd.setPublisher(addisonWesleySaved);
 
-        Author rodSaved = authorRepository.save(rod);
-        Book noEJBSaved = bookRepository.save(noEJB);
+    Author ericSaved = authorRepository.save(eric);
+    Book dddSaved = bookRepository.save(ddd);
+    addisonWesleySaved.getBooks().add(dddSaved);
 
-        ericSaved.getBooks().add(dddSaved);
-        rodSaved.getBooks().add(noEJBSaved);
+    // Add second author
+    Author rod = new Author();
+    rod.setFirstName("Rod");
+    rod.setLastName("Johnson");
 
-        authorRepository.save(ericSaved);
-        authorRepository.save(rodSaved);
+    // Add second book
+    Book noEJB = new Book();
+    noEJB.setTitle("J2EE Development without EJB");
+    noEJB.setIsbn("54757585");
+    noEJB.setPublisher(wroxSaved);
 
-        System.out.println("In Bootstrap");
-        System.out.println("Author Count: " + authorRepository.count());
-        System.out.println("Book Count: " + bookRepository.count());
+    Author rodSaved = authorRepository.save(rod);
+    Book noEJBSaved = bookRepository.save(noEJB);
+    wroxSaved.getBooks().add(noEJBSaved);
+
+    ericSaved.getBooks().add(dddSaved);
+    rodSaved.getBooks().add(noEJBSaved);
+
+    authorRepository.save(ericSaved);
+    authorRepository.save(rodSaved);
 
 
-    }
+    System.out.println("In Bootstrap");
+    System.out.println("Author Count: " + authorRepository.count());
+    System.out.println("Book Count: " + bookRepository.count());
+    System.out.println("Publisher Count: " + publisherRepository.count());
+  }
 }
 
 
--- src/main/java/guru/springframework/spring7webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring7webapp/domain/Book.java
@@ -9,75 +9,98 @@ import java.util.Set;
  * Created by jt, Spring Framework Guru.
  */
 @Entity
-public class Book {
+public class Book
+{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String title;
-    private String isbn;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String title;
+  private String isbn;
 
-    @ManyToMany
-    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
-        inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors = new HashSet<>();
+  @ManyToMany
+  @JoinTable(name = "author_book",
+    joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+  private Set<Author> authors = new HashSet<>();
 
-    public Set<Author> getAuthors() {
-        return authors;
+  @ManyToOne
+  @JoinColumn(name = "publisher_id")
+  private Publisher publisher;
+
+  public Long getId()
+  {
+    return id;
+  }
+
+  public void setId(Long id)
+  {
+    this.id = id;
+  }
+
+  public Set<Author> getAuthors()
+  {
+    return authors;
+  }
+
+  public void setAuthors(Set<Author> authors)
+  {
+    this.authors = authors;
+  }
+
+  public Publisher getPublisher()
+  {
+    return publisher;
+  }
+  public void setPublisher(Publisher publisher)
+  {
+    this.publisher = publisher;
+  }
+
+  public String getTitle()
+  {
+    return title;
+  }
+
+  public void setTitle(String title)
+  {
+    this.title = title;
+  }
+
+  public String getIsbn()
+  {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn)
+  {
+    this.isbn = isbn;
+  }
+
+  @Override
+  public String toString()
+  {
+    return "Book{" + "id=" + id + ", title='" + title + '\'' + ", isbn='" + isbn + '\'' + ", authors=" + authors + '}';
+  }
+
+  @Override
+  public boolean equals(Object o)
+  {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Book book)) {
+      return false;
     }
 
-    public void setAuthors(Set<Author> authors) {
-        this.authors = authors;
-    }
+    return getId() != null ? getId().equals(book.getId()) : book.getId() == null;
+  }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Book)) return false;
-
-        Book book = (Book) o;
-
-        return getId() != null ? getId().equals(book.getId()) : book.getId() == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return getId() != null ? getId().hashCode() : 0;
-    }
+  @Override
+  public int hashCode()
+  {
+    return getId() != null ? getId().hashCode() : 0;
+  }
 }
 
 
